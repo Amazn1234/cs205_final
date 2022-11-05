751c9023ca707de85af0c896a3c4e5885d1ac8f0
Amazn1 <you@example.com>
2022-11-04 21:22:39 -0700

    first commit adding file: finalScript1.sh

--- finalScript1.sh
+++ finalScript1.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+#cs 205_final script
+#!/bin/bash
+#prompts user for input for file name and sets it to variable FILE
+echo  "enter file name: "
+read FILE
+
+#prints title
+echo "=== SUMMARY OF DATA FILE ==="
+#prints file name
+echo -n "file name: " $FILE
+
+#total lines for total pokemon
+echo -n "total Pokemon: " 
+awk 'END {print NR}' $FILE
+
+#get average hp of all pokemon
+echo -n "Avg HP: "
+awk '{ sum += $6; n++ } END { if (n > 0 ) print  sum/n;}' $FILE
+
+#get average attack of all pokemon
+echo -n "Avg Attack: "
+awk '{ sum += $7; x++ } END {if (x>0) print sum / x; }' $FILE
+
+#print end script
+echo "=== END SUMMARY ==="
+
